d102501e77eca0f5fa2f375fe858ae26d0cfca92
UwaweTera <uwaweteraviv@gmail.com>
2025-05-12 16:43:25 +0200

    add cors

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const productRoutes = require('./products');
-const authRoutes = require('./auth');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const productRoutes = require("./products");
+const authRoutes = require("./auth");
+const jwt = require("jsonwebtoken");
 
 const app = express();
+app.use(cors()); // Enable CORS for all routes
 app.use(bodyParser.json());
 
-const SECRET_KEY = 'abefghijklmnopqrstuvwxyz1_234567890jdfyhgtuasjgfdsbfeadbmfjdfbvchjdbv_edhs';
+const SECRET_KEY =
+  "abefghijklmnopqrstuvwxyz1_234567890jdfyhgtuasjgfdsbfeadbmfjdfbvchjdbv_edhs";
 
 // Middleware to protect routes
 function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]; // 'Bearer TOKEN'
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // 'Bearer TOKEN'
 
   if (!token) return res.sendStatus(401);
 
@@ -24,12 +27,12 @@ function authenticateToken(req, res, next) {
   });
 }
 // Simple root route
-app.get('/', (req, res) => {
-  res.send('✅ Server is running!');
+app.get("/", (req, res) => {
+  res.send("✅ Server is running!");
 });
 
-app.use('/auth', authRoutes);
-app.use('/products', authenticateToken, productRoutes);
+app.use("/auth", authRoutes);
+app.use("/products", authenticateToken, productRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
